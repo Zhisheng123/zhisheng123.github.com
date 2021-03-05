4537dec4b9fc34c261ddf24387cec63e1cbffad3
Zhisheng Wang <zzzwang@ucdavis.edu>
2021-03-04 16:29:18 -0800

    home page created

---
+++ des157/Prototype/js/index.js
@@ -0,0 +1,79 @@
+(function(){
+	'use strict';
+	const leftImg = document.getElementById("left");
+	const centerImg = document.getElementById("center");
+	const rightImg = document.getElementById("right");
+	 window.onscroll = function() {
+	   var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	   console.log("滚动距离" + scrollTop);
+	   setImg(scrollTop);
+	 }
+	 
+	 
+	function setImg(scrollTopPost){
+
+		
+		let zoom=parseInt(centerImg.style.zoom);
+		let sideZoom=parseInt(centerImg.style.zoom);
+		let imgLeft = parseInt(centerImg.offsetLeft);
+		let topLeft = parseInt(centerImg.offsetTop);
+		let imgWidth = document.body.clientWidth;
+ 
+		if(isNaN(zoom)){
+			zoom=100;
+		}
+		if(isNaN(sideZoom)){
+			sideZoom=100;
+		}
+		if(scrollTopPost>200){
+			scrollTopPost = 200;
+		}
+		zoom = 100 - scrollTopPost*0.3;
+		sideZoom= 100 + scrollTopPost*0.3;
+		
+		leftImg.style.zoom=sideZoom+'%';
+		centerImg.style.zoom=zoom+'%';
+		rightImg.style.zoom=sideZoom+'%';
+	}
+	
+	leftImg.addEventListener('click', function(){
+		getImg(leftImg);
+	})
+	centerImg.addEventListener('click', function(){
+		getImg(centerImg);
+	})
+	rightImg.addEventListener('click', function(){
+		getImg(rightImg);
+	})
+	
+	function getImg(realimg){
+		let motai=document.getElementById('mo');
+		let moimg=document.getElementById("moimg");
+		let caption=document.getElementById("caption");
+		let brief = document.getElementById("brief");
+		let imgDesc1 = "Miami-Zhisheng Wang ,2020.MIAMI.";
+		let imgDesc2 = "Miami-Zhisheng Wang,2020.MIAMI.";
+		let imgDesc3 = "Miami-Zhisheng Wang,2020.MIAMI.";
+		realimg.onclick=function(){
+			motai.style.display="block"
+			moimg.src=this.src
+			caption.innerHTML=this.alt
+			if(moimg.src.includes("1.jpg")){
+				brief.innerHTML=imgDesc1;
+			}else if(moimg.src.includes("2.jpg")){
+				brief.innerHTML=imgDesc2;
+			}else{
+				brief.innerHTML=imgDesc3;
+			}
+		}
+		
+		let span=document.getElementById("close");
+		span.onclick=function(){
+			motai.style.display="none";
+		}
+		
+	}
+	
+	
+ 
+})();
